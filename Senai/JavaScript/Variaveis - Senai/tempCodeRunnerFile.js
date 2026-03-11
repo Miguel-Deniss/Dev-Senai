437320e4b86bbcd6cee72828457e064dd167e729
Miguel Denisovas <baiozin.denisovas@gmail.com>
2026-03-11 11:05:57 -0300

    Desafio

---
+++ Senai/JavaScript/Variaveis - Senai/tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+// Variáveis
+let nomeAluno = "João";
+let nota1 = 8;
+let nota2 = 7;
+
+let media = calcularMedia(nota1, nota2);
+
+console.log("Nome do aluno: ", nomeAluno);
+console.log("Nota 1: ", nota1);
+console.log("Nota 2: ", nota2);
+
+console.log("Média do aluno:", media);
+
